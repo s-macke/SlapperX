bcb685e7e65b11295100b5ac0d7db01aca077ed4
Sebastian Macke <sebastian@macke.de>
2024-08-07 23:19:57 +0200

    Separate attack method

--- src/targeter.go
+++ src/targeter.go
@@ -50,9 +50,93 @@ func (trgt *Targeter) nextRequest() *http.Request {
 	return &request
 }
 
-func (trgt *Targeter) attack(client *tracing.Client, ch <-chan time.Time) {
-	var dnsError *net.DNSError
+type AttackResponse struct {
+	status int
+	err    error
+	start  time.Time
+	end    time.Time
+}
 
+func (trgt *Targeter) DoRequest(client *tracing.Client, request *http.Request) AttackResponse {
+	attackResponse := AttackResponse{
+		status: 0,
+		err:    nil,
+	}
+	attackResponse.start = time.Now()
+
+	response, err := client.Do(request)
+	if err != nil && trgt.verbose {
+		fmt.Println("Error:", request.Method, request.URL, err)
+	}
+	if err == nil {
+		attackResponse.status = response.StatusCode
+		_, err = io.ReadAll(response.Body)
+		if err != nil && trgt.verbose {
+			fmt.Println("Error:", request.Method, request.URL, err)
+		}
+		_ = response.Body.Close()
+	}
+	attackResponse.err = err
+	attackResponse.end = time.Now()
+	return attackResponse
+}
+
+func FillStats(request *http.Request, response AttackResponse,
+	currentSetRate float64, currentInFlightRequests int64) {
+	var dnsError *net.DNSError
+	stats.responsesReceived.Add(1)
+	if response.err == nil {
+		stats.responses.status[response.status].Add(1)
+	} else {
+		switch {
+		case
+			errors.Is(response.err, io.EOF):
+			stats.responses.ErrorEof.Add(1)
+		case
+			errors.Is(response.err, syscall.ECONNREFUSED):
+			stats.responses.ErrorConnRefused.Add(1)
+		case
+			os.IsTimeout(response.err):
+			stats.responses.ErrorTimeout.Add(1)
+		case
+			errors.As(response.err, &dnsError):
+			stats.responses.ErrorNoSuchHost.Add(1)
+		default:
+			stats.responses.status[0].Add(1)
+		}
+	}
+
+	elapsed := response.end.Sub(response.start)
+	elapsedMs := float64(elapsed) / float64(time.Millisecond)
+	// to test the latency distribution
+	// elapsedMs = (math.Sin(elapsedMs)+1.1)*30. + math.Cos(float64(start.UnixMilli()/5000))*100 + 100.
+
+	if trgt.logFile != nil {
+		trgt.logFile.WriteString(
+			fmt.Sprintf("%s,%d,%d,%d,%d,%.1f\n",
+				response.start.Format("2006-01-02T15:04:05.999999999"),
+				response.start.Sub(trgt.attackStartTime).Milliseconds(),
+				elapsed.Milliseconds(),
+				response.status,
+				currentInFlightRequests,
+				currentSetRate))
+	}
+
+	if trgt.verbose {
+		fmt.Println(request.Method, request.URL, response.status, elapsedMs)
+		return
+	}
+
+	elapsedBucket := ui.lbc.calculateBucket(elapsedMs)
+	timings := stats.timings.getTimingsSlot(response.end) // end is basically now
+	if response.status >= 200 && response.status < 300 {
+		timings[elapsedBucket].Ok.Add(1)
+	} else {
+		timings[elapsedBucket].Bad.Add(1)
+	}
+}
+
+func (trgt *Targeter) attack(client *tracing.Client, ch <-chan time.Time) {
 	for {
 		_, ok := <-ch
 		if !ok { // channel closed
@@ -65,73 +149,8 @@ func (trgt *Targeter) attack(client *tracing.Client, ch <-chan time.Time) {
 		currentSetRate := stats.currentSetRate
 		currentInFlightRequests := stats.getInFlightRequests()
 
-		start := time.Now()
-		response, err := client.Do(request)
-		if err != nil && trgt.verbose {
-			fmt.Println("Error:", request.Method, request.URL, err)
-		}
-		if err == nil {
-			_, err = io.ReadAll(response.Body)
-			if err != nil && trgt.verbose {
-				fmt.Println("Error:", request.Method, request.URL, err)
-			}
-			_ = response.Body.Close()
-		}
-
-		now := time.Now()
-		elapsed := now.Sub(start)
-		elapsedMs := float64(elapsed) / float64(time.Millisecond)
-
-		stats.responsesReceived.Add(1)
-
-		status := 0
-		if err == nil {
-			status = response.StatusCode
-			stats.responses.status[status].Add(1)
-		} else {
-			switch {
-			case
-				errors.Is(err, io.EOF):
-				stats.responses.ErrorEof.Add(1)
-			case
-				errors.Is(err, syscall.ECONNREFUSED):
-				stats.responses.ErrorConnRefused.Add(1)
-			case
-				os.IsTimeout(err):
-				stats.responses.ErrorTimeout.Add(1)
-			case
-				errors.As(err, &dnsError):
-				stats.responses.ErrorNoSuchHost.Add(1)
-			default:
-				stats.responses.status[0].Add(1)
-			}
-		}
-
-		if trgt.logFile != nil {
-			trgt.logFile.WriteString(
-				fmt.Sprintf("%s,%d,%d,%d,%d,%.1f\n",
-					start.Format("2006-01-02T15:04:05.999999999"),
-					start.Sub(trgt.attackStartTime).Milliseconds(),
-					elapsed.Milliseconds(),
-					status,
-					currentInFlightRequests,
-					currentSetRate))
-		}
-
-		if trgt.verbose {
-			fmt.Println(request.Method, request.URL, status, elapsedMs)
-			continue
-		}
-		// to test the latency distribution
-		// elapsedMs = (math.Sin(elapsedMs)+1.1)*30. + math.Cos(float64(start.UnixMilli()/5000))*100 + 100.
-
-		elapsedBucket := ui.lbc.calculateBucket(elapsedMs)
-		timings := stats.timings.getTimingsSlot(now)
-		if status >= 200 && status < 300 {
-			timings[elapsedBucket].Ok.Add(1)
-		} else {
-			timings[elapsedBucket].Bad.Add(1)
-		}
+		response := trgt.DoRequest(client, request)
+		FillStats(request, response, currentSetRate, currentInFlightRequests)
 	}
 }
 
